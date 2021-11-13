6214b7cdac8d0d89338786abda546595d82c4f3d
luke <luc.dewit@hotmail.nl>
2021-11-13 23:18:15 +0100

    new argparse system

--- src/bcs.py
+++ src/bcs.py
@@ -1,55 +1,58 @@
 #!/usr/bin/env python3
-import sys
-from modules.usage import *
+
 from modules.run_program import *
 from modules.compile_program import *
 from modules.opcodes import *
-import subprocess
 from modules.parser import *
 
+import sys
+import subprocess
+import argparse
 
-def uncons(xs):
-    return (xs[0], xs[1:])
+# arg_parser.print_help()
+
+BCS_VERSION = "BCS Compiler/Interpreter V1.0.0"
+
+arg_parser = argparse.ArgumentParser(description='BeCause programming language compiler and interpreter')
+
+sysargs = sys.argv[1:]
+
+arg_parser.add_argument('filename', nargs='?', help='File with the source code to compile/interpret', default='')
+arg_parser.add_argument('-v', '--version', help='Show the version of this BCS installation', action='store_true')
+arg_parser.add_argument('-d', '--debug', help='Prevent deletion of output .asm file', action='store_true')
+arg_parser.add_argument('-r', '--run', help='Run the produced executable after compilation', action='store_true')
+arg_parser.add_argument('-i', '--interpret', help='Interpret the program in build-in interpreter instead of compiling it', action='store_true')
+arg_parser.add_argument("-o", "--output", metavar='<file>', help="Directs the output to a name of your choice")
+
+args = arg_parser.parse_args(sysargs)
+
+if not args.debug and not args.interpret and not args.run and not args.version and args.output == None:
+    print("test")
+    arg_parser.print_help()
+    exit(0)
+
+if args.version:
+    print(BCS_VERSION)
+    exit(0)
 
 
 if __name__ == "__main__":
-    (program_name, argv) = uncons(sys.argv)
-
-    if len(argv) < 1:
-        usage(program_name)
-        print("CLI Error: no subcommand provided\n")
-        exit(-1)
-
-    (subcommand, options) = uncons(argv)
-
-    debug = "debug" in options
-
-    if subcommand == "help":
-        if len(options) < 1:
-            usage(program_name)
-    elif subcommand == "run":
-        if len(options) < 1:
-            usage(program_name)
-            print("CLI Error: no filename given\n")
-            exit(-1)
-
-        (input_file_name, options) = uncons(options)
-        program = load_program_from_file(input_file_name)
+    if args.interpret:
+        program = load_program_from_file(args.filename)
         run_program(program)
-    elif subcommand == "com":
-        if len(options) < 1:
-            usage(program_name)
-            print("CLI Error: no filename given\n")
-            exit(-1)
 
-        (input_file_name, options) = uncons(options)
-        program = load_program_from_file(input_file_name)
-        compile_program_linux_x86_64(program, "output.asm")
-        subprocess.call(["nasm", "-felf64", "output.asm"])
-        subprocess.call(["ld", "-o", "output", "output.o"])
-        subprocess.call(["rm", "output.o"])
+    # elif subcommand == "com":
+    #     if len(options) < 1:
+    #         usage(program_name)
+    #         print("CLI Error: no filename given\n")
+    #         exit(-1)
 
-        if not debug:
-            subprocess.call(["rm", "output.asm"])
-    else:
-        print(f"CLI Error: no subcommand like '{subcommand}'")
+    #     (input_file_name, options) = uncons(options)
+    #     program = load_program_from_file(input_file_name)
+    #     compile_program_linux_x86_64(program, "output.asm")
+    #     subprocess.call(["nasm", "-felf64", "output.asm"])
+    #     subprocess.call(["ld", "-o", "output", "output.o"])
+    #     subprocess.call(["rm", "output.o"])
+
+    #     if not args.debug:
+    #         subprocess.call(["rm", "output.asm"])
--- src/modules/usage.py
+++
@@ -1,5 +0,0 @@
-def usage(program_name):
-    print(f"Usage: {program_name} <SUBCOMMAND> [ARGS]")
-    print("        help <file>     Show this help screen")
-    print("        run <file>      Interpret the program")
-    print("        com <file>      Compile the program\n")
