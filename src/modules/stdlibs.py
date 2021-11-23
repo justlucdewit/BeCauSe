72762f23f3e13601d41915dc288ebdfae0552090
luke <luc.dewit@hotmail.nl>
2021-11-23 10:14:42 +0100

    WIP read write system

--- src/modules/stdlibs.py
+++ src/modules/stdlibs.py
@@ -1,6 +1,6 @@
 stdlibs = {
     "std/io": open("modules/stdlib/io.bcs").read(),
-    "str/io": open("modules/stdlib/str.bcs").read(),
+    "mem/io": open("modules/stdlib/mem.bcs").read(),
     "sys/io": open("modules/stdlib/sys.bcs").read(),
     "time/io": open("modules/stdlib/time.bcs").read()
 }
